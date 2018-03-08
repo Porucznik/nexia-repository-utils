74e94d589a0c48d30da929b5507cd7d032d18e14
Porucznik <porucznik13@gmail.com>
2018-03-08 22:15:33 +0100

    finished setup script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,30 @@
 #!/usr/bin/env bash
-PACKAGES=("https://aur.archlinux.org/nitrux-icon-theme.git")
+
+# geting list of packages from packages.conf
+readarray -t PACKAGES < packages.conf
+
+# creating work directories
 mkdir -p Nexia\ Linux/repository/nexia-dev/
 mkdir -p work/
+
+# cloning packages git repos
 for package in "${PACKAGES[@]}"
 do
     DIR=${package#*//*/}
     DIR=${DIR%.git}
     DIR=${DIR#Porucznik/}
     DIR="work/$DIR"
-    mkdir -p $DIR
-    git clone $package $DIR
+    if [[ ! -d "$DIR" ]]; then
+        echo "Found new $DIR, creating"
+        mkdir -p $DIR
+        git clone $package $DIR
+        echo "$DIR created"
+    else
+        echo "Found existing $DIR, countinuing"
+    fi
 done
+
+# sync local repo, if run for the first time add -a before -s
+grive -s Nexia\ Linux/
+
+echo "Setup complete"
